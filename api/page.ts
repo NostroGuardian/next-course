2b1e83e08238384c11d2a911f553613b5d1a0731
nostroguardian <shmelivan5@yandex.ru>
2025-01-18 19:58:30 +0300

    (canceling error)

--- api/page.ts
+++ api/page.ts
@@ -2,13 +2,7 @@ import { API } from '@/app/api';
 import { TopPageModel } from '@/interfaces/page.interface';
 
 export async function getPage(alias: string): Promise<TopPageModel | null> {
-	throw new Error('err');
-	await new Promise((res) =>
-		setTimeout(() => {
-			res('');
-		}, 3000)
-	);
-	const res = await fetch(API.topPage.byAlias + alias + '/123123', {
+	const res = await fetch(API.topPage.byAlias + alias, {
 		next: { revalidate: 10 },
 	});
 	if (!res.ok) {
